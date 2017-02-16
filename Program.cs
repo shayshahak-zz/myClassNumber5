34e282526e7eba48ae2fe854ca3814bd3c15184f
shayshahak <shays@microsoft.com>
2017-02-16 19:47:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5InClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is loach ha kefel");
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write("{0}\t", i * j);
+                }
+                Console.WriteLine("\n");
+            }
+
+            // take name and while name different then my name the program will continue to run
+            string myString= null;
+            {
+                while (myString != "shay")
+                {
+                    Console.WriteLine("enter your name");
+                    myString = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
